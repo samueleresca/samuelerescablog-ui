ba8c1b97cb38594375cb59733d850af3209ff5fa
Samuele Resca <samuele.resca@gmail.com>
2020-12-05 21:07:17 +0000

    Adding service worker.

--- src/webpack.mix.js
+++ src/webpack.mix.js
@@ -1,20 +1,28 @@
 let mix = require('laravel-mix');
+require('laravel-mix-workbox');
 
 mix.webpackConfig({
   module: {
     rules: [
       {
         test: /\.jsx?$/,
-        use: [
-          {
-            loader: 'babel-loader',
-            options: Config.babel()
-          }
-        ]
+        loader: 'babel-loader',
+        options: Config.babel()
       }
     ]
+  },
+  output: {
+    publicPath: ''
   }
 });
+
+const assetsTransform = async (manifestEntries) => {
+  const manifest = manifestEntries.map(entry => {
+    entry.url = '/assets/' + entry.url;
+    return entry;
+  });
+  return {manifest, warnings: []};
+};
 
 mix.js('js/helpers.js', 'js/')
   .js('js/app.js', 'js/')
@@ -22,9 +30,13 @@ mix.js('js/helpers.js', 'js/')
   .js('js/post.js', 'js/')
   .js('js/page.js', 'js/')
   .extract()
-  .setResourceRoot('/assets')
   .setPublicPath('../assets')
-  .minify('js/vendor.js')
+  .setResourceRoot('/assets')
+  .injectManifest({
+    swSrc: './js/sw.js',
+    swDest: './js/sw.js',
+    manifestTransforms: [assetsTransform]
+  })
   .sass('sass/app.scss', 'css/')
   .options({
     processCssUrls: false
