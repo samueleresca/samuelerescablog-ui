5730b78c0ebc1559eb62763cc6df969e7fe9407d
Samuele Resca <samuele.resca@gmail.com>
2024-01-20 17:59:52 +0000

    Updating infra.

--- infra/deploy.js
+++ infra/deploy.js
@@ -8,9 +8,10 @@ const GhostAdminApi = require('@tryghost/admin-api');
             version: 'canary'
         });
 
+        console.log('Branch:' + precess.env.branch);
         // Deploy it to the configured site
-        await api.themes.upload({file: 'samueleresca-ui.zip'});
-        console.log('Theme successfully uploaded.');
+        //await api.themes.upload({file: 'samueleresca-ui.zip'});
+        //console.log('Theme successfully uploaded.');
     } catch (err) {
         console.error(err);
         console.error(err.errors.map(x=>x.message));
