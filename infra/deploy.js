6e7f9bfc0cf39f54b4e83c65bffed2e9ee35a767
Samuele Resca <samuele.resca@gmail.com>
2024-01-20 18:31:37 +0000

    Updating.

--- infra/deploy.js
+++ infra/deploy.js
@@ -1,18 +1,25 @@
-import GhostAdminAPI from '@tryghost/admin-api';
+import GhostAdminApi from '@tryghost/admin-api';
 
-try {
-    const api = new GhostAdminApi({
-        url: process.env.url,
-        key: process.env.key,
-        version: 'canary'
-    });
+async function main() {
+    try {
+        const api = new GhostAdminApi({
+            url: process.env.url,
+            key: process.env.key,
+            version: 'canary'
+        });
 
-    console.log('Branch:' + precess.env.branch);
-    // Deploy it to the configured site
-    //await api.themes.upload({file: 'samueleresca-ui.zip'});
-    //console.log('Theme successfully uploaded.');
-} catch (err) {
-    console.error(err);
-    console.error(err.errors.map(x=>x.message));
-    process.exit(1);
+        console.log("Branch name"+ process.env.branch);
+        // Deploy it to the configured site
+        //await api.themes.upload({ file: 'samueleresca-ui.zip' });
+        //console.log('Theme successfully uploaded.');
+    } catch (err) {
+        console.error(err);
+        console.error(err.errors.map(x => x.message));
+        process.exit(1);
+    }
+}
+
+// Execute the main function when the module is run
+if (require.main === module) {
+    main();
 }
