154dc904565e030293451899fa35453234a2af95
Samuele Resca <samuele.resca@gmail.com>
2024-01-20 18:12:50 +0000

    Updating.

--- infra/deploy.js
+++ infra/deploy.js
@@ -1,20 +1,18 @@
-const GhostAdminApi = require('@tryghost/admin-api');
+import GhostAdminAPI from '@tryghost/admin-api';
 
-(async function main() {
-    try {
-        const api = new GhostAdminApi({
-            url: process.env.url,
-            key: process.env.key,
-            version: 'canary'
-        });
+try {
+    const api = new GhostAdminApi({
+        url: process.env.url,
+        key: process.env.key,
+        version: 'canary'
+    });
 
-        console.log('Branch:' + precess.env.branch);
-        // Deploy it to the configured site
-        //await api.themes.upload({file: 'samueleresca-ui.zip'});
-        //console.log('Theme successfully uploaded.');
-    } catch (err) {
-        console.error(err);
-        console.error(err.errors.map(x=>x.message));
-        process.exit(1);
-    }
-}());
+    console.log('Branch:' + precess.env.branch);
+    // Deploy it to the configured site
+    //await api.themes.upload({file: 'samueleresca-ui.zip'});
+    //console.log('Theme successfully uploaded.');
+} catch (err) {
+    console.error(err);
+    console.error(err.errors.map(x=>x.message));
+    process.exit(1);
+}
